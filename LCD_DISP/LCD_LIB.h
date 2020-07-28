dbf23f423cfec42cb81e4140c06af44bca58eb6d
Mike <michalbaz@poczta.onet.pl>
2020-07-28 14:57:50 +0200

    init LCD_DISP

---
+++ LCD_DISP/LCD_LIB.h
@@ -0,0 +1,22 @@
+#ifndef LCD_LIB_h_
+#define LCD_LIB_h_
+#define RS 0
+#define PORT_RS D
+#define RW 1
+#define PORT_RW D
+#define E 2
+#define PORT_E D
+#define D7 5
+#define PORT_D7 D
+#define D6 6
+#define PORT_D6 D
+#define D5 7
+#define PORT_D5 D
+#define D4 0
+#define PORT_D4 B
+
+void init_display(void); //init I/O ports 
+void set_to_output(void); //set data ports to output
+void set_to_input(void); //set data ports to input
+
+#endif
---
+++ LCD_DISP/main.c
@@ -0,0 +1,13 @@
+#define F_CPU 8000000UL 
+#include <avr/io.h>
+#include <util/delay.h>
+#include "LCD_LIB.h"
+
+
+int main(void){
+    
+    while(1){
+
+    }
+    return 0;
+}
