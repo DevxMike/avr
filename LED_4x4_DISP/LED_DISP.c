c3c659798885736f96a974cf4bf2e24bd7240213
Mike <michalbaz@poczta.onet.pl>
2020-07-30 16:25:02 +0200

    inline functions

--- LED_4x4_DISP/LED_DISP.c
+++ LED_4x4_DISP/LED_DISP.c
@@ -1,9 +1,14 @@
-#include <avr/io.h>
+
 #include "LED_DISP.h"
 
 
 
 void init_led(void){
+    set_output();
+
+    
+}
+void set_output(void){
     DDR(PORT_DIG1) |= (1 << DIG1);
     DDR(PORT_DIG2) |= (1 << DIG2);
     DDR(PORT_DIG3) |= (1 << DIG3);
@@ -17,4 +22,7 @@ void init_led(void){
     DDR(PORT_F_PIN) |= (1 << F_PIN);
     DDR(PORT_G_PIN) |= (1 << G_PIN);
     DDR(PORT_DOT) |= (1 << DOT);
+}
+void demo(void){
+
 }
--- LED_4x4_DISP/LED_DISP.h
+++ LED_4x4_DISP/LED_DISP.h
@@ -1,6 +1,6 @@
 #ifndef LED_DISP_h_
 #define LED_DISP_h_
-
+#include <avr/io.h>
 #define PORT(x) SPORT(x) //those macros let us use some
 #define SPORT(x) (PORT##x) //convenient stuff like i.e. PORT(PORT_RS) |= (1 << RS) 
 #define DDR(x) SDDR(x) //instead of PORTx |= (1 << RS)
@@ -36,5 +36,79 @@
 
 void init_led(void);
 void set_output(void);
+void demo(void);
 
+inline void set_a(void){ //helpful inline functions to set or clear output (set 1 or 0)
+    PORT(PORT_A_PIN) |= (1 << A_PIN);
+}
+inline void set_b(void){
+    PORT(PORT_B_PIN) |= (1 << B_PIN);
+}
+inline void set_c(void){
+    PORT(PORT_C_PIN) |= (1 << C_PIN);
+}
+inline void set_d(void){
+    PORT(PORT_D_PIN) |= (1 << D_PIN);
+}
+inline void set_e(void){
+    PORT(PORT_E_PIN) |= (1 << E_PIN);
+}
+inline void set_f(void){
+    PORT(PORT_F_PIN) |= (1 << F_PIN);
+}
+inline void set_g(void){
+    PORT(PORT_G_PIN) |= (1 << G_PIN);
+}
+inline void set_dot(void){
+    PORT(PORT_DOT) |= (1 << DOT);
+}
+inline void set_dig1(void){
+    PORT(PORT_DIG1) |= (1 << DIG1);
+}
+inline void set_dig2(void){
+    PORT(PORT_DIG2) |= (1 << DIG2);
+}
+inline void set_dig3(void){
+    PORT(PORT_DIG3) |= (1 << DIG3);
+}
+inline void set_dig4(void){
+    PORT(PORT_DIG4) |= (1 << DIG4);
+}
+
+inline void clr_a(void){
+    PORT(PORT_A_PIN) &= ~(1 << A_PIN);
+}
+inline void clr_b(void){
+    PORT(PORT_B_PIN) &= ~(1 << B_PIN);
+}
+inline void clr_c(void){
+    PORT(PORT_C_PIN) &= ~(1 << C_PIN);
+}
+inline void clr_d(void){
+    PORT(PORT_D_PIN) &= ~(1 << D_PIN);
+}
+inline void clr_e(void){
+    PORT(PORT_E_PIN) &= ~(1 << E_PIN);
+}
+inline void clr_f(void){
+    PORT(PORT_F_PIN) &= ~(1 << F_PIN);
+}
+inline void clr_g(void){
+    PORT(PORT_G_PIN) &= ~(1 << G_PIN);
+}
+inline void clr_dot(void){
+    PORT(PORT_DOT) &= ~(1 << DOT);
+}
+inline void clr_dig1(void){
+    PORT(PORT_DIG1) &= ~(1 << DIG1);
+}
+inline void clr_dig2(void){
+    PORT(PORT_DIG2) &= ~(1 << DIG2);
+}
+inline void clr_dig3(void){
+    PORT(PORT_DIG3) &= ~(1 << DIG3);
+}
+inline void clr_dig4(void){
+    PORT(PORT_DIG4) &= ~(1 << DIG4);
+}
 #endif
