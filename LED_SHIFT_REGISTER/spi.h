96aa391c3a4c4f48a87e8a9c895e3b1b7bab3bcc
Mike <michalbaz@poczta.onet.pl>
2020-08-04 16:31:51 +0200

    SPI should be working

--- LED_SHIFT_REGISTER/spi.h
+++ LED_SHIFT_REGISTER/spi.h
@@ -1,4 +1,13 @@
 #ifndef spi_h
 #define spi_h
+#include "LED_DISP.h"
+#define SCK 5
+#define PORT_SCK B
+#define MOSI 3
+#define PORT_MOSI B
+#define SS 2
+#define PORT_SS B
 
+void init_spi_MASTER(void);
+void send_data(const uint8_t);
 #endif
