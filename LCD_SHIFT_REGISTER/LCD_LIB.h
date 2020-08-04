ca5dd49617b2cdd4d7a71df79dfcb8f460af9989
Mike <michalbaz@poczta.onet.pl>
2020-08-04 20:14:50 +0200

    simple display OK but locating RAM is not working

---
+++ LCD_SHIFT_REGISTER/LCD_LIB.h
@@ -0,0 +1,55 @@
+#include <stdbool.h>
+
+#ifndef LCD_LIB_h_
+#define LCD_LIB_h_
+
+#define RS 0 //RS is pin no. 0
+#define PORT_RS D //on PORTD
+#define RW 1 //and so on
+#define PORT_RW D
+#define E 2
+#define PORT_E D
+#define PORT_D4 B
+#define PORT(x) SPORT(x) //those macros let us use some
+#define SPORT(x) (PORT##x) //convenient stuff like i.e. PORT(PORT_RS) |= (1 << RS) 
+#define DDR(x) SDDR(x) //instead of PORTx |= (1 << RS)
+#define SDDR(x) (DDR##x)
+#define PIN(x) SPIN(x)
+#define SPIN(x) (PIN##x)
+
+
+#define CLEAR_DISP 0x01 //clear display command
+#define HOME 0x02 //return home command
+#define DISP_CTRL 0x0F //turn on display, turn on cursor, turn on it`s bilnikng
+    #define DISP_OFF 0xF7 //i.e. DISP_CTRL & DISP_OFF -> turn off the display mask
+    #define CURSOR_OFF 0xFD //turn off cursor mask
+    #define BLINK_OFF 0xFE //turn off blinking mask
+#define ENTRY_SET 0x07 //turn on increment, shift display
+    #define DECREMENT_ENTRY 0xFD //shift to the left while writing to the display
+    #define NO_DISPLAY_SHIFT 0xFE //dont shift to the left/right
+#define LOCATION_DDRAM 0x80 //DDRAM (display data ram) first element location
+#define SECOND_LINE 0x40 //address of the display`s second line
+#define LOCATION_CGRAM 0x40 //CGRAM (character generator ram) first element location
+
+void init_display(void); //init I/O ports 
+void write_byte(uint8_t); //writes 1 byte of data 
+void write_data_byte(char); //writes user`s data
+void write_instruction(uint8_t); //writes instruction
+void write_string(char*); //write string to the display
+void locate_ddram(uint8_t, uint8_t); //set location in ddram
+void locate_cgram(uint8_t); //set location in cgram
+void write_to_cgram(uint8_t,const uint8_t*, uint8_t); //write your own char pattern to cgram
+
+inline void set_rs(void){
+    PORT(PORT_RS) |= (1 << RS);
+}
+inline void set_e(void){
+    PORT(PORT_E) |= (1 << E);
+}
+inline void clear_rs(void){
+    PORT(PORT_RS) &= ~(1 << RS);
+}
+inline void clear_e(void){
+    PORT(PORT_E) &= ~(1 << E);
+}
+#endif
--- LCD_SHIFT_REGISTER/main.c
+++ LCD_SHIFT_REGISTER/main.c
@@ -1,9 +1,9 @@
 #include "timer.h"
-#include "LED_DISP.h"
 #include "spi.h"
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "shift_register.h"
+#include "LCD_LIB.h"
 
 uint8_t states[] = {
     0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90 //values that are going to be written to PORT_KATHODE reg to set 0 on certain pins
@@ -15,23 +15,18 @@ uint8_t seg = 0x01; //starting value for PORT_DIG (0x01 writes 1 to dig1 pin)
 
 int main(void){
     init_spi_MASTER(); //initialize SPI
-    init_timer(); //initialize timer
     init_register(); //initialize register
+    init_display();
+    write_instruction(DISP_CTRL & BLINK_OFF & CURSOR_OFF);
     sei(); //global interrupt enable
-    send_byte_to_reg(0x00);
+    write_string("string");
     while(1){
         
     }
     return 0;
 }
 
+/*
 ISR(TIMER2_COMP_vect){
-    //static uint8_t index = 0;
-    //send_byte_to_reg(states[disp_val[index++]]); //set PORT(PORT_KATHODES) value for disp_val that is indexed by index
-    //if(index > 3) index = 0;
-    PORT(PORT_DIG) &= 0xF0; //multiply by 0xF0 so we are not changing the values that segments are not connected with and at the same time, we are setting 
-                            //all dig`s to 0 so none of `em is active
-    PORT(PORT_DIG) |= seg; //set segment to 1
-    seg = seg << 1; //shift seg to the left by one place (multiply by 2)
-    if(seg > 8) seg = 0x01; //if seg > 8 that means it has reached place after the last pin that the digit is attached to so start again from 0x01
 }
+*/
